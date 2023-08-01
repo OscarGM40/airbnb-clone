503d3545d69fd348fb468e2a7b9abf1eb1dde7e8
Oscar <oscargm28@hotmail.com>
2023-08-01 16:35:45 +0200

    6h40m added react-date-range

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { SafeUser } from "@/app/types";
 import { Category } from "../navbar/Categories";
+import useCountries from "@/app/hooks/useCountries";
+import Avatar from "../shared/Avatar";
+import ListingCategory from "./ListingCategory";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../shared/Map"),{ssr:false});
 
 interface ListingInfoProps {
   user: SafeUser | null;
@@ -20,6 +26,37 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   bathroomCount,
   locationValue,
 }) => {
-  return <div>ListingInfo</div>;
+  const { getByValue } = useCountries();
+  const coordinates = getByValue(locationValue);
+
+  return (
+    <div className="col-span-4 flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-semibold flex flex-row items-center gap-2">
+          <div>Hosted by {user?.name}</div>
+          <Avatar src={user?.image} />
+        </div>
+        <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
+          <div>{guestCount} guests</div>
+          <div>{roomCount} rooms</div>
+          <div>{bathroomCount} bathrooms</div>
+        </div>
+      </div>
+      <hr className="mt-2 mb-1" />
+      {category && (
+        <ListingCategory
+          icon={category.icon}
+          label={category.label}
+          description={category.description}
+        />
+      )}
+      <hr className="mt-2 mb-1" />
+      <div className="text-lg font-light text-neutral-500">
+        {description}
+      </div>
+      <hr className="mt-2 mb-3" />
+      <Map center={coordinates?.latlng} />
+    </div>
+  );
 };
 export default ListingInfo;
